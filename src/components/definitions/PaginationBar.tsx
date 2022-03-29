d0ef8ce8b486c7b93511f3e8e433a98cd38aaa96
Aleksander <aleksandergorecki80@gmail.com>
2022-03-29 01:18:29 +0100

    Modyfy pagiantion bar

--- src/components/definitions/PaginationBar.tsx
+++ src/components/definitions/PaginationBar.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Theme } from '@mui/material/styles/createTheme';
+
 
 interface ChildProps {
   count: number;
@@ -11,6 +14,8 @@ const PaginationBar: React.FC<ChildProps> = ({ count, setPage }) => {
   const onChangeHandler = (event: React.ChangeEvent<unknown>, page: number) => {
     setPage(page);
   };
+  const isSmallScreen = useMediaQuery((theme: Theme) => theme.breakpoints.down("sm"));
+
 
   return (
     <Stack
@@ -18,10 +23,11 @@ const PaginationBar: React.FC<ChildProps> = ({ count, setPage }) => {
     >
       <Pagination
         count={count}
-        showFirstButton
-        showLastButton
+        showFirstButton={isSmallScreen ? false : true}
+        showLastButton={isSmallScreen ? false : true}
+        boundaryCount={isSmallScreen ? 0 : 2}
         onChange={onChangeHandler}
-        size="small"
+        size={!isSmallScreen ? "small" : "medium"}
       />
     </Stack>
   );
